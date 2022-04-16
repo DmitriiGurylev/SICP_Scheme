5a29f3c9dc115a41c37afb9693a5ec7dfb168eb4
Wonderful-guy <gurylev.dmitrii@mail.ru>
2022-04-16 17:31:40 +0300

    some tasks refactored, some tasks added

--- 1.26 task.rkt
+++ 1.26 task.rkt
@@ -27,11 +27,18 @@
 (define (expmod base exp m)
   (cond ((= exp 0) 1)
         ((even? exp)
-         (remainder (square (expmod base (/ exp 2) m))
+;         (remainder (square (expmod base (/ exp 2) m))
+;          m))
+         (remainder 
+          (* (expmod base (/ exp 2) m)
+             (expmod base (/ exp 2) m))
           m))
         (else
          (remainder (* base (expmod base (- exp 1) m))
-          m))))
+                    m))))
+
+
+        
 
 (define (square n) (* n n))
 
--- 1.27 task.rkt
+++ 1.27 task.rkt
@@ -2,21 +2,48 @@
 
 (define (square n) (* n n))
 
+(define (fermat-test n)
+  (define (try-it a)
+    (= (expmod a n n) a))
+  (try-it (+ 1 (random (- n 1)))))
+
+(define (fast-prime? n times)
+  (cond
+    ((= times 0) true)
+    ((fermat-test n) (fast-prime? n (- times 1)))
+    (else false)))
+
 (define (expmod base exp m)
   (cond ((= exp 0) 1)
         ((even? exp)
          (remainder (square (expmod base (/ exp 2) m))
-          m))
+          m))      
         (else
          (remainder (* base (expmod base (- exp 1) m))
-          m)))) 
+                    m))))       
 
-(define (fast-prime? n current-time)
-  (define (try-it a) (= (expmod a n n) a))
-  (define (fermat-test n) (try-it current-time))
+(define (timed-prime-test n)
+  (start-prime-test n (runtime)))
 
-  (if (= (- n 1) current-time)
-      (display "prime!")
-      (if (fermat-test n)
-          (fast-prime? n (+ current-time 1))
-          (display "not prime!"))))
+(define (start-prime-test n start-time)
+  (if (fast-prime? n (- n 1))
+      (report-prime n (- (runtime) start-time))
+      false))
+
+(define (report-prime n elapsed-time)
+  (display true)
+  (display " *** ")
+  (display elapsed-time)
+  (newline))
+
+(define (runtime) (current-inexact-milliseconds))
+
+#|
+(list 
+   (timed-prime-test 561)
+   (timed-prime-test 1105)
+   (timed-prime-test 1729)
+   (timed-prime-test 2465)
+   (timed-prime-test 2821)
+   (timed-prime-test 6601))
+|#
